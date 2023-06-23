4b0e441822873cfe44d1121468acbd7e4a8d9bdf
kamrulSakurabo <kamrul.sakurabo@gmail.com>
2023-06-23 12:54:06 +0900

    aded

--- meo_api/settings.py
+++ meo_api/settings.py
@@ -14,7 +14,7 @@ SECRET_KEY = config('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = config('DEBUG', default=False, cast=bool)
 
-ALLOWED_HOSTS = ['meo-tool.work']
+ALLOWED_HOSTS = ['meo-tool.work', '133.18.226.253']
 
 
 # Application definition
